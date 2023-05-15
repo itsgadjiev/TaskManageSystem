1d16119285bc341bfa2557b57a3608f803b3afdd
JekaH1 <ceyhun100203@gmail.com>
2023-05-16 00:50:36 +0400

    non optimized reflection messega sender done

--- TaskManagament/LoginRegConsole/LoginRegConsole/Services/MessageService.cs
+++ TaskManagament/LoginRegConsole/LoginRegConsole/Services/MessageService.cs
@@ -7,6 +7,7 @@ using SendGrid;
 using SendGrid.Helpers.Mail;
 using System.Globalization;
 using System.Reflection;
+using System.Reflection.Metadata;
 using Content = LoginRegConsole.Database.Models.Content;
 
 namespace LoginRegConsole.Services
@@ -42,26 +43,16 @@ namespace LoginRegConsole.Services
 
 		private Content PrepareContentForBlogCommentEmail(BlogComment blogComment)
 		{
+			MessageTemplate messageTemplate = new MessageTemplate();
+
 			Content content = new Content();
 			PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
-
+			Type type = messageTemplate.GetType();
+			FieldInfo field = default;
 			foreach (PropertyInfo propertyInfo in propsOfContent)
 			{
-				if (propertyInfo.Name == "CONTENT_AZ")
-				{
-					propertyInfo.SetValue(content, PrepareMessageForBlogComment(MessageTemplate.BLOG_COMMENT_SENDED_AZ, blogComment));
-					continue;
-				}
-				else if (propertyInfo.Name == "CONTENT_RU")
-				{
-					propertyInfo.SetValue(content, PrepareMessageForBlogComment(MessageTemplate.BLOG_COMMENT_SENDED_RU, blogComment));
-					continue;
-				}
-				else if (propertyInfo.Name == "CONTENT_EN")
-				{
-					propertyInfo.SetValue(content, PrepareMessageForBlogComment(MessageTemplate.BLOG_COMMENT_SENDED_EN, blogComment));
-					continue;
-				}
+				field = type.GetField("BLOG_COMMENT_SENDED_" + propertyInfo.Name.Substring(propertyInfo.Name.Length - 2, 2))!;
+				propertyInfo.SetValue(content, PrepareMessageForBlogComment((string)field.GetValue(messageTemplate), blogComment));
 			}
 
 			return content;
@@ -75,188 +66,133 @@ namespace LoginRegConsole.Services
 		#region PrepareContentForBlogEmailDueStatus
 		private Content PrepareContentForBlogEmail(Blog blog)
 		{
+			MessageTemplate messageTemplate = new MessageTemplate();
 			Content content = new Content();
 			PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
+			Type type = messageTemplate.GetType();
+			FieldInfo field = default;
 
-			if (blog.BlogStatus == BlogStatus.APPROVED)
+			foreach (PropertyInfo propertyInfo in propsOfContent)
 			{
-				foreach (PropertyInfo propertyInfo in propsOfContent)
-				{
-					if (propertyInfo.Name == "CONTENT_AZ")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_AZ, blog));
-						continue;
-					}
-					else if (propertyInfo.Name == "CONTENT_RU")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_RU, blog));
-						continue;
-					}
-					else if (propertyInfo.Name == "CONTENT_EN")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_EN, blog));
-						continue;
-					}
-				}
-			}
-			else if (blog.BlogStatus == BlogStatus.REJECTED)
-			{
-				foreach (PropertyInfo propertyInfo in propsOfContent)
-				{
-					if (propertyInfo.Name == "CONTENT_AZ")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_AZ, blog));
-						continue;
-					}
-					else if (propertyInfo.Name == "CONTENT_RU")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_RU, blog));
-						continue;
-					}
-					else if (propertyInfo.Name == "CONTENT_EN")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_EN, blog));
-						continue;
-					}
-				}
-			}
-			else if (blog.BlogStatus == BlogStatus.WAITING)
-			{
-				foreach (PropertyInfo propertyInfo in propsOfContent)
-				{
-					if (propertyInfo.Name == "CONTENT_AZ")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_AZ, blog));
-						continue;
-					}
-					else if (propertyInfo.Name == "CONTENT_RU")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_RU, blog));
-						continue;
-					}
-					else if (propertyInfo.Name == "CONTENT_EN")
-					{
-						propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_EN, blog));
-						continue;
-					}
-				}
-			}
+				string language = propertyInfo.Name.Substring(propertyInfo.Name.Length - 2, 2);
+				field = type.GetField(blog.BlogStatus.ToString() + "_BLOG_" + propertyInfo.Name.Substring(propertyInfo.Name.Length - 2, 2))!;
+				propertyInfo.SetValue(content, PrepareMessageForBlog((string)field.GetValue(messageTemplate), blog));
 
+			}
 			return content;
 		}
+			#endregion
 
-		#endregion
+			#region SeperateMethods
+			//private Content PrepareContentForAcceptedBlogEmail(Blog blog)
+			//{
+			//	Content content = new Content();
+			//	PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
 
-		#region SeperateMethods
-		//private Content PrepareContentForAcceptedBlogEmail(Blog blog)
-		//{
-		//	Content content = new Content();
-		//	PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
+			//	foreach (PropertyInfo propertyInfo in propsOfContent)
+			//	{
+			//		if (propertyInfo.Name == "CONTENT_AZ")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_AZ, blog));
+			//			continue;
+			//		}
+			//		else if (propertyInfo.Name == "CONTENT_RU")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_RU, blog));
+			//			continue;
+			//		}
+			//		else if (propertyInfo.Name == "CONTENT_EN")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_EN, blog));
+			//			continue;
+			//		}
+			//	}
 
-		//	foreach (PropertyInfo propertyInfo in propsOfContent)
-		//	{
-		//		if (propertyInfo.Name == "CONTENT_AZ")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_AZ, blog));
-		//			continue;
-		//		}
-		//		else if (propertyInfo.Name == "CONTENT_RU")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_RU, blog));
-		//			continue;
-		//		}
-		//		else if (propertyInfo.Name == "CONTENT_EN")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.APPROVED_BLOG_EN, blog));
-		//			continue;
-		//		}
-		//	}
+			//	return content;
+			//}
+			//private Content PrepareContentForRejetedBlogEmail(Blog blog)
+			//{
+			//	Content content = new Content();
+			//	PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
 
-		//	return content;
-		//}
-		//private Content PrepareContentForRejetedBlogEmail(Blog blog)
-		//{
-		//	Content content = new Content();
-		//	PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
+			//	foreach (PropertyInfo propertyInfo in propsOfContent)
+			//	{
+			//		if (propertyInfo.Name == "CONTENT_AZ")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_AZ, blog));
+			//			continue;
+			//		}
+			//		else if (propertyInfo.Name == "CONTENT_RU")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_RU, blog));
+			//			continue;
+			//		}
+			//		else if (propertyInfo.Name == "CONTENT_EN")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_EN, blog));
+			//			continue;
+			//		}
+			//	}
 
-		//	foreach (PropertyInfo propertyInfo in propsOfContent)
-		//	{
-		//		if (propertyInfo.Name == "CONTENT_AZ")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_AZ, blog));
-		//			continue;
-		//		}
-		//		else if (propertyInfo.Name == "CONTENT_RU")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_RU, blog));
-		//			continue;
-		//		}
-		//		else if (propertyInfo.Name == "CONTENT_EN")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.REJECTED_BLOG_EN, blog));
-		//			continue;
-		//		}
-		//	}
+			//	return content;
+			//}
+			//private Content PrepareContentForWaitingBlogEmail(Blog blog)
+			//{
+			//	Content content = new Content();
+			//	PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
 
-		//	return content;
-		//}
-		//private Content PrepareContentForWaitingBlogEmail(Blog blog)
-		//{
-		//	Content content = new Content();
-		//	PropertyInfo[] propsOfContent = LocalizationService.GetPropertiesOfEntry(content);
+			//	foreach (PropertyInfo propertyInfo in propsOfContent)
+			//	{
+			//		if (propertyInfo.Name == "CONTENT_AZ")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_AZ, blog));
+			//			continue;
+			//		}
+			//		else if (propertyInfo.Name == "CONTENT_RU")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_RU, blog));
+			//			continue;
+			//		}
+			//		else if (propertyInfo.Name == "CONTENT_EN")
+			//		{
+			//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_EN, blog));
+			//			continue;
+			//		}
+			//	}
 
-		//	foreach (PropertyInfo propertyInfo in propsOfContent)
-		//	{
-		//		if (propertyInfo.Name == "CONTENT_AZ")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_AZ, blog));
-		//			continue;
-		//		}
-		//		else if (propertyInfo.Name == "CONTENT_RU")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_RU, blog));
-		//			continue;
-		//		}
-		//		else if (propertyInfo.Name == "CONTENT_EN")
-		//		{
-		//			propertyInfo.SetValue(content, PrepareMessageForBlog(MessageTemplate.WAITING_BLOG_EN, blog));
-		//			continue;
-		//		}
-		//	}
+			//	return content;
+			//}
+			#endregion
 
-		//	return content;
-		//}
-		#endregion
-
-		public void SendMessage(User user, Content content)
-		{
-			Email sendingMessage = new Email(content, UserService.ActiveUser, user);
-			_messageRepository.Add(sendingMessage);
-		}
-		public void SendMessageForBlogDueStatus(Blog blog)
-		{
-			SendMessage(blog.PostingUser, PrepareContentForBlogEmail(blog));
-		}
-		public static async Task SendMessageDueStatusForBlogIRL(Blog blog)
-		{
-			MessageService messageService = new MessageService();
-			MessageTemplate messageTemplate = new MessageTemplate();
-			Type type = messageTemplate.GetType();
-			FieldInfo field = type.GetField(blog.BlogStatus.ToString() + "_BLOG_" + "EN")!;
-			string apiKey = Environment.GetEnvironmentVariable(KeyForMessageService.API_KEY);
-			var client = new SendGridClient(apiKey);
-			var msg = new SendGridMessage()
+			public void SendMessage(User user, Content content)
 			{
-				From = new EmailAddress(KeyForMessageService.MESSAGE_SENDER_EMAIL, KeyForMessageService.MESSAGE_SENDER_FULLNAME),
-				Subject = $"Blog status",
-				PlainTextContent = $"{messageService.PrepareMessageForBlog((string)field.GetValue(messageTemplate), blog)}",
-			};
-			msg.AddTo(new EmailAddress($"{blog.PostingUser.Email}", $"{blog.PostingUser.Name}"));
+				Email sendingMessage = new Email(content, UserService.ActiveUser, user);
+				_messageRepository.Add(sendingMessage);
+			}
+			public void SendMessageForBlogDueStatus(Blog blog)
+			{
+				SendMessage(blog.PostingUser, PrepareContentForBlogEmail(blog));
+			}
+			public static async Task SendMessageDueStatusForBlogIRL(Blog blog)
+			{
+				MessageService messageService = new MessageService();
+				MessageTemplate messageTemplate = new MessageTemplate();
+				Type type = messageTemplate.GetType();
+				FieldInfo field = type.GetField(blog.BlogStatus.ToString() + "_BLOG_" + "EN")!;
+				string apiKey = Environment.GetEnvironmentVariable(KeyForMessageService.API_KEY);
+				var client = new SendGridClient(apiKey);
+				var msg = new SendGridMessage()
+				{
+					From = new EmailAddress(KeyForMessageService.MESSAGE_SENDER_EMAIL, KeyForMessageService.MESSAGE_SENDER_FULLNAME),
+					Subject = $"Blog status",
+					PlainTextContent = $"{messageService.PrepareMessageForBlog((string)field.GetValue(messageTemplate), blog)}",
+				};
+				msg.AddTo(new EmailAddress($"{blog.PostingUser.Email}", $"{blog.PostingUser.Name}"));
 
-			var response = await client.SendEmailAsync(msg);
-			
+				var response = await client.SendEmailAsync(msg);
+
+
+			}
 
 		}
-
 	}
-}
